18b61f4ad6d4146f6899c843414b5aec24926ab0
adam <afenton@getmoxied.net>
2016-07-28 17:39:03 -0500

    Add header and footer

---
+++ footer.php
@@ -0,0 +1,17 @@
+<?php namespace Lean;
+/**
+ * The template for displaying the footer.
+ *
+ * @package Lean
+ * @since 1.0.0
+ */
+
+use Timber\Timber;
+
+?>
+
+<?php Timber::render( '02-organisms/00-global/01-footer.twig' ) ?>
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,27 @@
+<?php namespace Lean;
+
+use Timber\Timber;
+
+/**
+ * The main header file.
+ *
+ * @package Lean
+ * @since 1.0.0
+ */
+
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+<?php Timber::render( '02-organisms/00-global/00-header.twig' ) ?>
