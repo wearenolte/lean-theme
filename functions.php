728fa4f476641c7aba911a36828fd538d0731c1c
adam <afenton@getmoxied.net>
2016-07-28 10:26:54 -0500

    Include js and css assets

--- functions.php
+++ functions.php
@@ -19,19 +19,6 @@ $class_name::init();
 
 
 
-
-
-
-
-
-
-//
-//
-//
-//
-//use Lean\Config;
-//use Lean\Assets;
-//
 ///**
 // * Action to load the configuration file and define constant values, this action
 // * has priority of 1 in order to be executed before any other action using the
@@ -53,50 +40,3 @@ $class_name::init();
 //
 //}, 1);
 //
-///**
-// * Action created to add theme supports or specific of the theme.
-// */
-//add_action( 'after_setup_theme', function(){
-//	add_theme_support( 'post-thumbnails' );
-//});
-//
-///**
-// * Action that adds the styles from the theme to the editor in order to see the
-// * same styles from the front end in the dashboard on the editor section.
-// */
-//add_action( 'after_setup_theme', function(){
-//	if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
-//		add_editor_style( _THEME_PATH_ . '/assets/css/style.css' );
-//	} else {
-//		add_editor_style( _THEME_PATH_ . '/assets/css/style-min.css' );
-//	}
-//});
-//
-///**
-// * Action that is responsable for register the menus used on the theme.
-// */
-//add_action( 'after_setup_theme', function(){
-//	register_nav_menu(
-//		'primary-navigation',
-//		__( 'Primary Menu', _TEXT_DOMAIN_ )
-//	);
-//});
-//
-///**
-// * This actions register the assets and some configuration about the assets
-// * in order to load the CSS and JS files into the theme.
-// */
-//add_action( 'after_setup_theme', function() {
-//	$args = [
-//		'css_version' => false,
-//		'js_version' => time(),
-//		'theme_path' => _THEME_URL_,
-//	];
-//	$assets = new Assets( $args );
-//	$assets->load();
-//});
-//
-//add_filter('loader_alias', function( $alias ) {
-//	$alias['partial'] = 'partials';
-//	return $alias;
-//});
---
+++ src/Assets.php
@@ -0,0 +1,30 @@
+<?php namespace Leanp;
+
+/**
+ * Set-up Assets.
+ */
+class Assets
+{
+	/**
+	 * Init.
+	 */
+	public static function init() {
+		add_action( 'after_setup_theme', [ __CLASS__, 'init_assets' ] );
+	}
+
+	/**
+	 * Init the CSS and JS assets.
+	 */
+	public static function init_assets() {
+		$assets = new \Lean\Assets( [
+			'css_uri' => get_template_directory_uri() . '/patterns/public/css/style.css',
+			'css_version' => time(),
+			'js_uri' => get_template_directory_uri() . '/xxx/script.js',
+			'js_version' => time(),
+			'jquery_uri' => '//code.jquery.com/jquery-2.2.4.min.js',
+			'jquery_version' => '2.2.4',
+		] );
+
+		$assets->load();
+	}
+}
