29059db1a87c94afbca3c90a22dd426c329c6b0d
adam <afenton@getmoxied.net>
2016-08-24 17:10:30 -0500

    Enqueue flickity scripts when required.

--- page-templates/flexible.php
+++ page-templates/flexible.php
@@ -1,11 +1,17 @@
 <?php /* Template Name: Flexible */
 
-use \Timber\Timber;
-
-get_header();
+use Timber\Timber;
+use LeanNs\Assets;
 
 $data = Timber::get_context();
 $data['post'] = \LeanNs\Patterns::get_post();
+
+if ( 'images' === $data['post']->acf['hero']['hero_background']['type'] ) {
+	Assets::enqueue_flickity();
+}
+
+get_header();
+
 Timber::render( 'templates/page-flexible.twig', $data );
 
 get_footer();
