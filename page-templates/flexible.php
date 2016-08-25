88c0e6af1f47959b7185f1851daf0d65767b3614
adam <afenton@getmoxied.net>
2016-08-25 15:27:19 -0500

    Exception if hero bg type is missing

--- page-templates/flexible.php
+++ page-templates/flexible.php
@@ -6,6 +6,10 @@ use LeanNs\Assets;
 $data = Timber::get_context();
 $data['post'] = \LeanNs\Patterns::get_post();
 
+if ( ! isset(  $data['post']->acf['hero']['hero_background']['type'] ) ) {
+	throw new Exception( 'acf[\'hero\'][\'hero_background\'][\'type\'] not found.' );
+}
+
 if ( 'images' === $data['post']->acf['hero']['hero_background']['type'] ) {
 	Assets::enqueue_flickity();
 }
