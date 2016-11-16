9f3b7ebb464aced3af64a6c2ae63dbf7a6ab4f11
mitogh <mitogh@gmail.com>
2016-11-15 20:21:31 -0600

    Remove automatic load of sass files - #23

--- patterns/js/main.js
+++ patterns/js/main.js
@@ -1,12 +1,3 @@
 'use strict';
 
-// First require the base components
-require.context( 'atoms/global/base', true, /^\.\/.*\.scss/ );
-
-// Then all atoms, molecules, etc
-require.context( 'atoms', true, /^\.\/.*\.scss/ );
-require.context( 'molecules', true, /^\.\/.*\.scss/ );
-require.context( 'organisms', true, /^\.\/.*\.scss/ );
-require.context( 'templates', true, /^\.\/.*\.scss/ );
-
 require( 'templates/layouts/site' )();
