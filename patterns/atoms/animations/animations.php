5c619ffc1c1db079be9910b2219f2223bcfaf520
mitogh <mitogh@gmail.com>
2016-11-16 22:01:05 -0600

    Convert files into PHP - #23

--- patterns/atoms/animations/animations.php
+++ patterns/atoms/animations/animations.php
@@ -1,9 +1,9 @@
 <div class="demo-animate"
-     onclick="this.classList.toggle( 'a__anim__bounce-in-right' )">
-    <strong>Bounce In Right</strong> Click to see the effect
+	 onclick="this.classList.toggle( 'a__anim__bounce-in-right' )">
+	<strong>Bounce In Right</strong> Click to see the effect
 </div>
 
 <div class="demo-animate"
-     onclick="this.classList.toggle( 'a__anim__bounce-out-left' )">
-    <strong>Bounce Out Left</strong> Click to see the effect
+	 onclick="this.classList.toggle( 'a__anim__bounce-out-left' )">
+	<strong>Bounce Out Left</strong> Click to see the effect
 </div>
