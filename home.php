07fd7c2cdf9ff13f38af6c80d4165d8fd3fab177
mitogh <mitogh@gmail.com>
2016-11-17 10:55:41 -0600

    Remove not required content

--- home.php
+++ home.php
@@ -5,11 +5,4 @@ use Lean\Load;
 
 get_header();
 
-echo '<h1>Sample!</h1>';
-
-Load::organisms( 'global/header' );
-Load::organisms( 'article/article-body' );
-Load::organisms( 'sections/latest-posts' );
-Load::organisms( 'global/footer' );
-
 get_footer();
