3e94ab788e8aa4ac91262a6d5db04b5a609f7f5e
mitogh <mitogh@gmail.com>
2016-12-06 07:39:48 -0600

    Exclude bower_components from the JS build - #11

--- patterns/gulpfile.js
+++ patterns/gulpfile.js
@@ -183,7 +183,7 @@ function jsBuild() {
   ];
   let loaders = [{
     test: /\.js$/,
-    exclude: /node_modules/,
+    exclude: /(node_modules|bower_components)/,
     loader: "babel-loader",
     query: {
       presets: ['es2015']
