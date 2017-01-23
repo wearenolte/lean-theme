946004b8dc42143c1c4e2ec8bcd899581bf24a9d
mitogh <mitogh@gmail.com>
2017-01-22 23:33:47 -0600

    Add colors file

---
+++ patterns/atoms/colors/colors.php
@@ -0,0 +1,54 @@
+<ul class="sg-colors">
+	<li>
+		<span class="sg-swatch a__color__brand-primary"></span>
+		<span class="sg-label">brand_primary</span>
+	</li>
+	<li>
+		<span class="sg-swatch a__color__brand-secondary"></span>
+		<span class="sg-label">brand_secondary</span>
+	</li>
+	<li>
+		<span class="sg-swatch a__color__brand-tertiary"></span>
+		<span class="sg-label">brand_tertiary</span>
+	</li>
+	<li>
+		<span class="sg-swatch a__color__black"></span>
+		<span class="sg-label">black</span>
+	</li>
+	<li>
+		<span class="sg-swatch a__color__white"></span>
+		<span class="sg-label">white</span>
+	</li>
+	<li>
+		<span class="sg-swatch a__color__gray_24"></span>
+		<span class="sg-label">gray_24</span>
+	</li>
+	<li>
+		<span class="sg-swatch a__color__gray_mid"></span>
+		<span class="sg-label">gray_mid</span>
+	</li>
+	<li>
+		<span class="sg-swatch a__color__white_smoke"></span>
+		<span class="sg-label">white_smoke</span>
+	</li>
+	<li>
+		<span class="sg-swatch a__color__error"></span>
+		<span class="sg-label">error</span>
+	</li>
+	<li>
+		<span class="sg-swatch a__color__positive"></span>
+		<span class="sg-label">positive</span>
+	</li>
+	<li>
+		<span class="sg-swatch a__color__neutral"></span>
+		<span class="sg-label">neutral</span>
+	</li>
+	<li>
+		<span class="sg-swatch a__color__overlay_dark"></span>
+		<span class="sg-label">overlay_dark</span>
+	</li>
+	<li>
+		<span class="sg-swatch a__color__overlay_light"></span>
+		<span class="sg-label">overlay_light</span>
+	</li>
+</ul>
