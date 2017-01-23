587a2e8c4a1813fe29234f17106f1886c737c08e
mitogh <mitogh@gmail.com>
2017-01-23 14:58:50 -0600

    Place support of theme on functions.php - Closes #29

--- functions.php
+++ functions.php
@@ -66,3 +66,10 @@ add_action( 'lean/after_footer', function(){
 		 // @codingStandardsIgnoreEnd
 	}
 });
+
+add_action( 'after_setup_theme', function() {
+	add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'title-tag' );
+	add_theme_support( 'html5' );
+});
+
