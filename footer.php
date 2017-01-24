3e52ae713cc7f952fd9ecddec221fd98f5cfd9d6
mitogh <mitogh@gmail.com>
2017-01-24 02:19:41 -0600

    Update footer logic to use the pattern

--- footer.php
+++ footer.php
@@ -5,14 +5,12 @@
  * @package Lean
  * @since 1.0.0
  */
+
+use Lean\Load;
 ?>
 
 <?php do_action( 'lean/before_footer' ); ?>
-<footer role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
-	<?php use_icon( 'twitter' ); ?>
-	<?php use_icon( 'facebook' ); ?>
-	<?php use_icon( 'instagram' ); ?>
-</footer>
+<?php Load::organisms( 'footer/footer' ); ?>
 <?php wp_footer(); ?>
 <?php do_action( 'lean/after_footer' ); ?>
 
