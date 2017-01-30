78f4c53f8d32e20a057766f7dec53410d0c782f8
raul <marreror91@gmail.com>
2017-01-30 17:07:47 -0500

    Fix last php lint issue

--- patterns/atoms/buttons/link-as-button.php
+++ patterns/atoms/buttons/link-as-button.php
@@ -9,6 +9,6 @@ $class = trim( implode( ' ', (array) $args['class'] ) );
 ?>
 
 <a href="<?php echo esc_url( $args['url'] ); ?>"
-   class="a__btn <?php echo esc_attr( $class ); ?>">
+	class="a__btn <?php echo esc_attr( $class ); ?>">
 	<?php echo esc_html( $args['label'] ); ?>
 </a>
