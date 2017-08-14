aa72c6d35a72ca9ccef8f0c8995e5cc7de9066ce
mitogh <mitogh@gmail.com>
2017-08-14 05:26:36 -0500

    Update styles to remove code that is not required - #42

--- patterns/atoms/images/image.php
+++ patterns/atoms/images/image.php
@@ -2,7 +2,7 @@
 $args = wp_parse_args( $args, [
 	'src' => '',
 	'alt' => '',
-	'class' => 'a__img__avatar',
+	'class' => '',
 ]);
 
 // Don't render if the params does not provide a source argument.
