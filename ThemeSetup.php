73dfb3b94755111d93959b93567e4c2256728f59
Francisco Giraldo <fgiraldo@wearenolte.com>
2019-06-19 16:08:08 -0500

    Fixes php error

--- ThemeSetup.php
+++ ThemeSetup.php
@@ -53,11 +53,13 @@ class ThemeSetup {
 	 * Dependency checks
 	 */
 	public static function check_dependencies() {
-		if ( is_plugin_active( 'advanced-custom-fields-pro/acf.php' ) ) {
-			return;
-		}
-
 		if ( is_admin() ) :
+			require_once ABSPATH . 'wp-admin/includes/plugin.php';
+
+			if ( is_plugin_active( 'advanced-custom-fields-pro/acf.php' ) ) {
+				return;
+			}
+
 			add_action(
 				'admin_notices',
 				function () {
