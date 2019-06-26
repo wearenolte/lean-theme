bc7498cd2f4261d37057c8159b705eda06dd4702
Francisco Giraldo <fgiraldo@wearenolte.com>
2019-06-26 09:06:24 -0500

    Added taxonomy creation library and example

---
+++ backend/WP/Taxonomies/Example.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Lean\WP\Taxonomies;
+
+use Lean\Taxonomy;
+
+/**
+ * Taxonomy example.
+ */
+class Example {
+	const NAME        = 'TaxExample';
+	const NAME_PLURAL = 'TaxExamples';
+	const SLUG        = 'taxexample';
+
+	/**
+	 * Class loaded automatically.
+	 */
+	public static function init() {
+		/** Remove this comment to enable the taxonomy.
+		 * add_action( 'init', [ __CLASS__, 'create_taxonomy' ] );
+		 */
+	}
+
+	/**
+	 * Create Category.
+	 */
+	public static function create_taxonomy() {
+		$tax = new Taxonomy(
+			[
+				'name'     => self::NAME,
+				'singular' => self::NAME,
+				'plural'   => self::NAME_PLURAL,
+				'slug'     => self::SLUG,
+				'objects'  => [
+					'post',
+				],
+				'args'     => [
+					'hierarchical'      => true,
+					'show_admin_column' => true,
+					'capabilities'      => [
+						'manage_terms' => 'manage_categories',
+						'edit_terms'   => 'manage_categories',
+						'delete_terms' => 'manage_categories',
+						'assign_terms' => 'edit_posts',
+					],
+				],
+			]
+		);
+		$tax->init();
+	}
+}
