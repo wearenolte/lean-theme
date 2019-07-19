5bf040d5cf278935297798a72bd470f9c971ba00
Francisco Giraldo <fgiraldo@wearenolte.com>
2019-07-19 11:29:02 -0500

    User should be able to zoom the site

--- header.php
+++ header.php
@@ -12,7 +12,7 @@ use Lean\Load;
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<?php wp_head(); ?>
