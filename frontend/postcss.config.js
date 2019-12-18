4ec6de76c4d433fbe1138b18ade549fc7050fde1
adam <afenton@getmoxied.net>
2019-12-18 15:09:23 -0600

    Merge in stashed changes

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -16,6 +16,7 @@ var purgecss = require('@fullhuman/postcss-purgecss')({
   defaultExtractor: content => content.match(/[A-Za-z0-9-_\.%:/]+/g) || []
 })
 
+
 module.exports = {
   plugins: [
     require( 'tailwindcss' ),
