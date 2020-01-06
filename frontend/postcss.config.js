a4b238a7c0fbc61d7fa7c23be0c09d646cc5c5ae
adam <afenton@getmoxied.net>
2020-01-06 15:36:33 -0600

    Nested CSS and refactor into smaller files

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -20,11 +20,10 @@ var purgecss = require('@fullhuman/postcss-purgecss')({
   defaultExtractor: content => content.match(/[A-Za-z0-9-_\.%:/]+/g) || []
 })
 
-
 module.exports = {
   plugins: [
     require( 'tailwindcss' ),
-    //require( 'postcss-nested' ),
+    require( 'postcss-nested' ),
     require( 'autoprefixer' ),
     ...process.env.NODE_ENV === 'production'
       ? [ purgecss ]
